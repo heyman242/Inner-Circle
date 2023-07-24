66982a15e5a49fa6513a2eadb7dde903dbcefeb4
Himanshu Bobade <himanshubobade96@gmail.com>
2023-07-24 23:11:29 +0530

    backend added login api using bcrypt

--- server/index.js
+++ server/index.js
@@ -8,7 +8,8 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
-import {register} from "./controllers/auth.js"
+import authRoutes from "./routes/auth.js";
+import { register } from "./controllers/auth.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -39,6 +40,9 @@ const upload = multer({ storage });
 
 /* ROUTES WITH FILE */
 app.post("/auth/register", upload.single("picture"), register);
+
+/* ROUTES */
+app.use('/auth', authRoutes);
 
 /*MONGOOSE SETUP*/
 
