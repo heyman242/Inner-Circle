c83917d4fe32f5019f66ab0c987e84a652fdeb01
Himanshu Bobade <himanshubobade96@gmail.com>
2023-07-25 18:07:29 +0530

    backend added user routes and users api

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
 import { register } from "./controllers/auth.js";
 
 const __filename = fileURLToPath(import.meta.url);
@@ -42,7 +43,8 @@ const upload = multer({ storage });
 app.post("/auth/register", upload.single("picture"), register);
 
 /* ROUTES */
-app.use('/auth', authRoutes);
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
 
 /*MONGOOSE SETUP*/
 
